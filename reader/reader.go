9f230df6da606c26e763f443a613bed810b67966
Phat Ho <hhphat@apcs.vn>
2020-01-09 14:44:23 +0700

    Refractor and adding header to csv

--- reader/reader.go
+++ reader/reader.go
@@ -5,17 +5,35 @@ import (
 	DataFormat "github.com/DeKal/costa-rewrite/dataformat"
 	Utils "github.com/DeKal/costa-rewrite/utils"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
 
-// ReadSearchTermsFromExcel from file
+const (
+	rating      = "Autocorrect Rating"
+	labelResult = "Event Label"
+	hk          = "HK"
+	id          = "ID"
+	my          = "MY"
+	ph          = "PH"
+	sh          = "SG"
+	tw          = "TW"
+)
+
+// ReadSearchTermsFromExcel from file excel
 func ReadSearchTermsFromExcel(inputCsvFileName string) []DataFormat.AutoCorrectRow {
 	file, _ := os.Open(inputCsvFileName)
 	defer file.Close()
 
 	csvr := csv.NewReader(file)
+	csvHeader := ReadHeader(csvr)
+	log.Println(csvHeader)
+	return ReadSearchTerms(csvr, csvHeader)
+}
 
+// ReadSearchTerms from file
+func ReadSearchTerms(csvr *csv.Reader, fieldMap map[string]int) []DataFormat.AutoCorrectRow {
 	autoCorrects := []DataFormat.AutoCorrectRow{}
 	for {
 		row, err := csvr.Read()
@@ -23,21 +41,43 @@ func ReadSearchTermsFromExcel(inputCsvFileName string) []DataFormat.AutoCorrectR
 			return autoCorrects
 		}
 		if isSG(row) {
-			autoCorrect := DataFormat.AutoCorrectRow{}
-			autoCorrect.Rating = row[0]
 
-			searchAndCorrectedTerm := strings.Split(row[1], ">")
-			autoCorrect.OriginSearchTerm = Utils.TrimLeftRightSpace(searchAndCorrectedTerm[0])
-
-			autoCorrect.OriginCorrectTerm = ""
-			if len(searchAndCorrectedTerm) == 2 {
-				autoCorrect.OriginCorrectTerm = Utils.TrimLeftRightSpace(searchAndCorrectedTerm[1])
+			searchTerm, correctedTerm := SplitResult(row[fieldMap[labelResult]])
+			autoCorrect := DataFormat.AutoCorrectRow{
+				Rating:            row[fieldMap[rating]],
+				OriginSearchTerm:  searchTerm,
+				OriginCorrectTerm: correctedTerm,
 			}
-
 			autoCorrects = append(autoCorrects, autoCorrect)
 		}
 
 	}
+}
+
+// SplitResult split result by >
+func SplitResult(result string) (string, string) {
+	searchAndCorrectedTerm := strings.Split(result, ">")
+	searchTerm := Utils.TrimLeftRightSpace(searchAndCorrectedTerm[0])
+	correctedTerm := ""
+	if len(searchAndCorrectedTerm) == 2 {
+		correctedTerm = Utils.TrimLeftRightSpace(searchAndCorrectedTerm[1])
+	}
+
+	return searchTerm, correctedTerm
+}
+
+// ReadHeader readheader from csv file and return a field map
+func ReadHeader(csvr *csv.Reader) map[string]int {
+	fieldMap := map[string]int{}
+	header, err := csvr.Read()
+	if err == io.EOF {
+		return fieldMap
+	}
+	for index, field := range header {
+		fieldMap[field] = index
+	}
+
+	return fieldMap
 }
 
 func isSG(row []string) bool {
