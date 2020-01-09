fb59ca621a9d2dbd9dd8fabf70f0206515d70560
Phat Ho <hhphat@apcs.vn>
2020-01-09 16:31:37 +0700

    Update evaluation label

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -15,14 +15,18 @@ func FormatCsvRow(response DataFormat.RewriteResponse, autoCorrectRow DataFormat
 			response.SearchTerm,
 			response.CorrectTerm,
 			strconv.FormatInt(int64(response.Count), 10),
-			evaluationLabel(autoCorrectRow.OriginCorrectTerm == response.CorrectTerm),
+			evaluationLabel(autoCorrectRow.OriginCorrectTerm, response.CorrectTerm),
 		},
 	}
 }
 
-func evaluationLabel(isExpectedEqualAnalyzing bool) string {
+func evaluationLabel(originCorrectTerm string, correctTerm string) string {
+	isExpectedEqualAnalyzing := originCorrectTerm == correctTerm
+	if len(correctTerm) == 0 {
+		return ""
+	}
 	if isExpectedEqualAnalyzing {
 		return "Correct Label"
 	}
-	return "Wrong Label"
+	return "Different Label"
 }
